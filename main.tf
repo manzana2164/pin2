cd4712ce84ae31f7371e68c13afdd0f3e9a0578f
Emilio Pascutti <111916983+manzana2164@users.noreply.github.com>
2024-12-19 12:17:36 -0300

    Update main.tf terraform providers

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.10.2"
+}
+
 provider "aws" {
   region = var.aws_region
 }
